d995f6060be3d6cbb0d9f47decdc927eb52d45a5
kevmgb <kithinjimkevin@gmail.com>
2022-11-25 16:51:56 +0300

    Add ThrottleFilter webflux component

--- src/main/java/com/example/ratelimitingdemo/config/RequestThrottleFilter.java
+++ src/main/java/com/example/ratelimitingdemo/config/RequestThrottleFilter.java
@@ -3,51 +3,42 @@ package com.example.ratelimitingdemo.config;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.benmanes.caffeine.cache.LoadingCache;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
 
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 
 @Component
-public class RequestThrottleFilter implements Filter {
+class RequestThrottleFilter implements WebFilter {
 
-    private final LoadingCache<String, Integer> requestCountsPerIpAddress;
+    private LoadingCache<String, Integer> requestCountsPerIpAddress;
 
     public RequestThrottleFilter(){
         super();
         requestCountsPerIpAddress = Caffeine.newBuilder().
-                expireAfterWrite(1, TimeUnit.SECONDS).build(key -> 0);
-    }
-
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
-            throws IOException, ServletException {
-        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
-        String clientIpAddress = getClientIP((HttpServletRequest) servletRequest);
-
-        if(isMaximumRequestsPerSecondExceeded(clientIpAddress)){
-            httpServletResponse.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
-            httpServletResponse.getWriter().write("Too many requests");
-            return;
-        }
-        filterChain.doFilter(servletRequest, servletResponse);
+                expireAfterWrite(1, TimeUnit.MINUTES).build(key -> 0);
     }
 
     private boolean isMaximumRequestsPerSecondExceeded(String clientIpAddress){
-        int maxRequestsPerSecond = 5;
         Integer requests;
         requests = requestCountsPerIpAddress.get(clientIpAddress);
 
         if (requests != null){
+            int maxRequestsPerSecond = 5;
             if (requests > maxRequestsPerSecond) {
                 requestCountsPerIpAddress.asMap().remove(clientIpAddress);
                 requestCountsPerIpAddress.put(clientIpAddress, requests);
                 return true;
             }
+
         } else {
             requests = 0;
         }
@@ -56,11 +47,25 @@ public class RequestThrottleFilter implements Filter {
         return false;
     }
 
-    public String getClientIP(HttpServletRequest request) {
-        String xfHeader = request.getHeader("X-Forwarded-For");
+    public String getClientIP(ServerHttpRequest request) {
+        String xfHeader = Objects.requireNonNull(request.getHeaders().get("X-Forwarded-For")).get(0);
         if (xfHeader == null){
-            return request.getRemoteAddr();
+            return request.getLocalAddress().toString();
         }
         return xfHeader.split(",")[0];
+    }
+
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain filterChain) {
+        ServerHttpRequest httpServletRequest = exchange.getRequest();
+        ServerHttpResponse httpServletResponse = exchange.getResponse();
+        String clientIpAddress = getClientIP(httpServletRequest);
+
+        if (isMaximumRequestsPerSecondExceeded(clientIpAddress)){
+            httpServletResponse.setStatusCode(HttpStatus.TOO_MANY_REQUESTS);
+            return Mono.empty();
+        }
+        return filterChain.filter(exchange);
     }
 }
